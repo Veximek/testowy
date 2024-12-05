2314b91d843529d5be39ec060f31cd23456b3e4f
vexim <vexim2002@gmail.com>
2024-12-05 11:01:34 +0100

    master

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,12 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
+
 
     }
 }
